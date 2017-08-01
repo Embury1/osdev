a4e7bd98ed91c8713467a163240675cf7657888a
Embury1 <patrik.sundstrom@gmail.com>
2017-08-01 16:46:15 +0200

    first commit

---
+++ kmain.c
@@ -0,0 +1,19 @@
+void kmain()
+{
+    return;
+}
+
+// example configuration bytes
+// struct example {
+//     unsigned char config;
+//     unsigned short address;
+//     unsigned char index;
+// } __attribute__((packed));
+
+// example function
+// int sum_of_three(int arg1, int arg2, int arg3)
+// {
+//     return arg1 + arg2 + arg3;
+// }
+
+// gcc: -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -Wall -Wextra -Werror
