1c92337ae88a012b9575e382ab74ab45ca3f9e1e
Embury1 <patrik.sundstrom@gmail.com>
2017-08-03 08:57:10 +0200

    basic writing to framebuffer

--- kmain.c
+++ kmain.c
@@ -1,19 +1,48 @@
-void kmain()
+#define FB_BLACK            0
+#define FB_BLUE             1
+#define FB_GREEN            2
+#define FB_CYAN             3
+#define FB_RED              4
+#define FB_MAGENTA          5
+#define FB_BROWN            6
+#define FB_LIGHT_GREY       7
+#define FB_DARK_GREY        8
+#define FB_LIGHT_BLUE       9
+#define FB_LIGHT_GREEN      10
+#define FB_LIGHT_CYAN       11
+#define FB_LIGHT_RED        12
+#define FB_LIGHT_MAGENTA    13
+#define FB_LIGHT_BROWN      14
+#define FB_WHITE            15
+
+#define FB_COLUMNS          80
+#define FB_ROWS             25
+
+char *fb = (char *) 0x000B8000;
+
+void fb_clear()
 {
-    return;
+    int i;
+    for (i = 0; i < FB_COLUMNS * FB_ROWS * 2; ++i)
+        fb[i] = 0;
 }
 
-// example configuration bytes
-// struct example {
-//     unsigned char config;
-//     unsigned short address;
-//     unsigned char index;
-// } __attribute__((packed));
+void fb_write_cell(unsigned int i, char c, unsigned char bg, unsigned char fg)
+{
+    fb[i * 2] = c;
+    fb[i * 2 + 1] = ((bg & 0x0F) << 4) | (fg & 0x0F);
+}
 
-// example function
-// int sum_of_three(int arg1, int arg2, int arg3)
-// {
-//     return arg1 + arg2 + arg3;
-// }
+void fb_write(char *c, unsigned char bg, unsigned char fg)
+{
+    unsigned int i = 0;
+    char *p = 0;
+    while (*(p = c + i++) != '\0')
+        fb_write_cell(i, *p, bg, fg);
+}
 
-// gcc: -m32 -nostdlib -nostdinc -fno-builtin -fno-stack-protector -nostartfiles -nodefaultlibs -Wall -Wextra -Werror
+void kmain()
+{
+    fb_clear();
+    fb_write("EmbOS", FB_BLACK, FB_LIGHT_BLUE);
+}
--- loader.s
+++ loader.s
@@ -1,22 +1,12 @@
 global loader                   ; the entry symbol for ELF
+global kernel_stack             ; the address of the kernel stack
+
+extern kmain
 
 MAGIC_NUMBER equ 0x1BADB002     ; define the magic number constant
 FLAGS        equ 0x0            ; multiboot flags
 CHECKSUM     equ -MAGIC_NUMBER  ; calculate the checksum
                                 ; (magic number + checksum + flags should equal 0)
-
-section .text:                  ; start of the text (code) section
-align 4                         ; the code must be 4 byte aligned
-    dd MAGIC_NUMBER             ; write the magic number to the machine code,
-    dd FLAGS                    ; the flags,
-    dd CHECKSUM                 ; and the checksum
-
-loader:                         ; the loader label (defined as entry point in linker script)
-    mov eax, 0xCAFEBABE         ; place the number 0xCAFEBABE in the register eax
-.loop:
-    jmp .loop                   ; loop forever
-
-extern kmain
 
 KERNEL_STACK_SIZE       equ 4096                ; size of stack in bytes
 
@@ -24,7 +14,19 @@ section .bss
 align 4                                         ; align at 4 bytes
 kernel_stack:                                   ; label points to beginning of memory
     resb KERNEL_STACK_SIZE                      ; reserve stack for the kernel_stack
+
+section .text                   ; start of the text (code) section
+align 4                         ; the code must be 4 byte aligned
+    dd MAGIC_NUMBER             ; write the magic number to the machine code,
+    dd FLAGS                    ; the flags,
+    dd CHECKSUM                 ; and the checksum
+
+loader:                         ; the loader label (defined as entry point in linker script)
     mov esp, kernel_stack + KERNEL_STACK_SIZE   ; point esp to the start of the
                                                 ; stack (end of memory area)
 
+enter_kmain:
     call kmain
+
+hang:
+    jmp hang
