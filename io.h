6acb13fe4786d866032866499c93c97a0e863a6e
Embury1 <patrik.sundstrom@gmail.com>
2017-08-03 17:22:13 +0200

    output through I/O port and text cursor

---
+++ io.h
@@ -0,0 +1,12 @@
+#ifndef _IO_H_
+#define _IO_H_
+
+/** outb:
+*   Send the given data to the given I/O port. Defined in io.s
+*
+*   @param port The I/O port to send the data to
+*   @param data The data to send to the I/O port
+*/
+void outb(unsigned short port, unsigned char data);
+
+#endif
--- kmain.c
+++ kmain.c
@@ -1,22 +1,30 @@
-#define FB_BLACK            0
-#define FB_BLUE             1
-#define FB_GREEN            2
-#define FB_CYAN             3
-#define FB_RED              4
-#define FB_MAGENTA          5
-#define FB_BROWN            6
-#define FB_LIGHT_GREY       7
-#define FB_DARK_GREY        8
-#define FB_LIGHT_BLUE       9
-#define FB_LIGHT_GREEN      10
-#define FB_LIGHT_CYAN       11
-#define FB_LIGHT_RED        12
-#define FB_LIGHT_MAGENTA    13
-#define FB_LIGHT_BROWN      14
-#define FB_WHITE            15
+#include "io.h"
 
-#define FB_COLUMNS          80
-#define FB_ROWS             25
+#define FB_BLACK                0
+#define FB_BLUE                 1
+#define FB_GREEN                2
+#define FB_CYAN                 3
+#define FB_RED                  4
+#define FB_MAGENTA              5
+#define FB_BROWN                6
+#define FB_LIGHT_GREY           7
+#define FB_DARK_GREY            8
+#define FB_LIGHT_BLUE           9
+#define FB_LIGHT_GREEN          10
+#define FB_LIGHT_CYAN           11
+#define FB_LIGHT_RED            12
+#define FB_LIGHT_MAGENTA        13
+#define FB_LIGHT_BROWN          14
+#define FB_WHITE                15
+
+#define FB_COLUMNS              80
+#define FB_ROWS                 25
+
+#define FB_COMMAND_PORT         0x3D4
+#define FB_DATA_PORT            0x3D5
+
+#define FB_HIGH_BYTE_COMMAND    14
+#define FB_LOW_BYTE_COMMAND     15
 
 char *fb = (char *) 0x000B8000;
 
@@ -37,12 +45,21 @@ void fb_write(char *c, unsigned char bg, unsigned char fg)
 {
     unsigned int i = 0;
     char *p = 0;
-    while (*(p = c + i++) != '\0')
-        fb_write_cell(i, *p, bg, fg);
+    while (*(p = c + i) != '\0')
+        fb_write_cell(i++, *p, bg, fg);
+}
+
+void fb_move_cursor(unsigned short pos)
+{
+    outb(FB_COMMAND_PORT,   FB_HIGH_BYTE_COMMAND);
+    outb(FB_DATA_PORT,      ((pos >> 8) & 0x00FF));
+    outb(FB_COMMAND_PORT,   FB_LOW_BYTE_COMMAND);
+    outb(FB_DATA_PORT,      pos & 0x00FF);
 }
 
 void kmain()
 {
     fb_clear();
-    fb_write("EmbOS", FB_BLACK, FB_LIGHT_BLUE);
+    fb_write("Welcome to EmbOS", FB_BLACK, FB_LIGHT_BLUE);
+    fb_move_cursor(80);
 }
