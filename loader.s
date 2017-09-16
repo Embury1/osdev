56924b2023a4e3bd82f0c36dab0a03b8029c5600
Embury1 <patrik.sundstrom@gmail.com>
2017-09-16 19:34:17 +0200

    move multiboot header to its own section

--- loader.s
+++ loader.s
@@ -16,11 +16,13 @@ align 4                                                 ; align at 4 bytes
 kernel_stack:                                           ; label points to beginning of memory
     resb KERNEL_STACK_SIZE                              ; reserve stack for the kernel_stack
 
-section .text                                           ; start of the text (code) section
+section .__mbHeader
 align 4                                                 ; the code must be 4 byte aligned
     dd MAGIC_NUMBER                                     ; write the magic number to the machine code,
     dd ALIGN_MODULES                                    ; align modules,
     dd CHECKSUM                                         ; and the checksum
+
+section .text
 
 loader:                                                 ; the loader label (defined as entry point in linker script)
     mov esp, kernel_stack + KERNEL_STACK_SIZE           ; point esp to the start of the
